9b7462ed1f00596ae94aba3e631fe8924f119813
Лавров Сергей Александрович <s.lavrov@asntl.ru>
2017-08-10 17:42:10 +0300

    Продолжение работы.

---
+++ src/bases/Array.java
@@ -0,0 +1,24 @@
+package bases;
+
+import java.util.Arrays;
+
+public class Array {
+
+    int[] arr1 = new int[5];
+    int arr[] = new int[3];
+
+    int[] arr2 = { 5, 10, 15, 20 };
+    int[][] arr3 = new int[10][20]; //двумерный массив
+
+
+    public void run() {
+        System.out.println(Arrays.toString(arr2));
+
+        int[][] table = new int[3][]; //нерегурярный массив
+        table[0] = new int[1];
+        table[1] = new int[5];
+        table[2] = new int[3];
+
+        int lengthOfArr2 = arr2.length; //длина массива
+    }
+}
